115e53f085b66277719eefbfa4f79809b7a1d393
dennisppaul <dennis.paul@hfk-bremen.de>
2020-05-22 20:05:26 +0200

    fixed boolean operations

--- processing-library/gewebe/src/de/hfkbremen/gewebe/CGALAlphaShape3.java
+++ processing-library/gewebe/src/de/hfkbremen/gewebe/CGALAlphaShape3.java
@@ -2,6 +2,8 @@ package de.hfkbremen.gewebe;
 
 import processing.core.PVector;
 
+import java.lang.reflect.Field;
+
 /**
  * http://doc.cgal.org/latest/Alpha_shapes_3/
  * <p>
@@ -11,6 +13,18 @@ import processing.core.PVector;
 public class CGALAlphaShape3 {
 
     static {
+        // append path to `java.library.path`
+        System.setProperty("java.library.path", System.getProperty("java.library.path") + ":" + "/path/to/libs");
+        try {
+            Field fieldSysPath = null;
+            fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+            fieldSysPath.setAccessible(true);
+            fieldSysPath.set(null, null);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(System.getProperty("java.library.path"));
         System.out.print("### loading native lib `" + CGALAlphaShape3.class.getName() + "` ...");
         System.loadLibrary(CGALAlphaShape3.class.getSimpleName());
         System.out.println(" ok");
@@ -18,12 +32,66 @@ public class CGALAlphaShape3 {
 
     private long ptr2cgalAlphaShape;
 
+    public void compute_cgal_alpha_shape(float[] pts_coord) {
+        ptr2cgalAlphaShape = init_alpha_shape(pts_coord);
+    }
+
+    public void compute_cgal_alpha_shape(PVector[] vertices) {
+        float[] pts_coord = new float[vertices.length * 3];
+        for (int i = 0; i < vertices.length; i++) {
+            pts_coord[3 * i + 0] = vertices[i].x;
+            pts_coord[3 * i + 1] = vertices[i].y;
+            pts_coord[3 * i + 2] = vertices[i].z;
+        }
+
+        ptr2cgalAlphaShape = init_alpha_shape(pts_coord);
+    }
+
+    public Mesh mesh(float m_alpha) {
+        return new Mesh(get_alpha_shape_mesh("REGULAR", m_alpha, ptr2cgalAlphaShape));
+    }
+
+    public float[] compute_regular_mesh(float m_alpha) {
+        return get_alpha_shape_mesh("REGULAR", m_alpha, ptr2cgalAlphaShape);
+    }
+
+    public float[] compute_singular_mesh(float m_alpha) {
+        return get_alpha_shape_mesh("SINGULAR", m_alpha, ptr2cgalAlphaShape);
+    }
+
+    public float[] compute_regular_mesh_optimal(int number_of_solid_components) {
+        return get_alpha_shape_mesh("REGULAR",
+                                    get_optimal_alpha(number_of_solid_components, ptr2cgalAlphaShape),
+                                    ptr2cgalAlphaShape);
+    }
+
+    public float[] compute_singular_mesh_optimal(int number_of_solid_components) {
+        return get_alpha_shape_mesh("SINGULAR",
+                                    get_optimal_alpha(number_of_solid_components, ptr2cgalAlphaShape),
+                                    ptr2cgalAlphaShape);
+    }
+
+    public float alpha() {
+        return get_alpha(ptr2cgalAlphaShape);
+    }
+
+
+    /* --- */
+
+    public int number_of_solid_components() {
+        return number_of_solid_components(ptr2cgalAlphaShape);
+    }
+
+    public float get_optimal_alpha(int nb_sc) {
+        return get_optimal_alpha(nb_sc, ptr2cgalAlphaShape);
+    }
+
     /**
      * print version of library.
      *
      * @return
      */
-    native int version();
+    public native int version();
 
     /**
      * Initialize the alpha_shape from the array of points coordinates
@@ -86,7 +154,7 @@ public class CGALAlphaShape3 {
      * @param ptr
      * @return
      */
-    private native float[] get_alpha_shape_mesh(String classification_type, float alpha, long ptr);
+    public native float[] get_alpha_shape_mesh(String classification_type, float alpha, long ptr);
 
     /**
      * @param nb_sc
@@ -94,58 +162,4 @@ public class CGALAlphaShape3 {
      * @return
      */
     private native float get_optimal_alpha(int nb_sc, long ptr);
-
-
-    /* --- */
-
-    public void compute_cgal_alpha_shape(float[] pts_coord) {
-        ptr2cgalAlphaShape = init_alpha_shape(pts_coord);
-    }
-
-    public void compute_cgal_alpha_shape(PVector[] vertices) {
-        float[] pts_coord = new float[vertices.length * 3];
-        for (int i = 0; i < vertices.length; i++) {
-            pts_coord[3 * i + 0] = vertices[i].x;
-            pts_coord[3 * i + 1] = vertices[i].y;
-            pts_coord[3 * i + 2] = vertices[i].z;
-        }
-
-        ptr2cgalAlphaShape = init_alpha_shape(pts_coord);
-    }
-
-    public Mesh mesh(float m_alpha) {
-        return new Mesh(get_alpha_shape_mesh("REGULAR", m_alpha, ptr2cgalAlphaShape));
-    }
-
-    public float[] compute_regular_mesh(float m_alpha) {
-        return get_alpha_shape_mesh("REGULAR", m_alpha, ptr2cgalAlphaShape);
-    }
-
-    public float[] compute_singular_mesh(float m_alpha) {
-        return get_alpha_shape_mesh("SINGULAR", m_alpha, ptr2cgalAlphaShape);
-    }
-
-    public float[] compute_regular_mesh_optimal(int number_of_solid_components) {
-        return get_alpha_shape_mesh("REGULAR",
-                                    get_optimal_alpha(number_of_solid_components, ptr2cgalAlphaShape),
-                                    ptr2cgalAlphaShape);
-    }
-
-    public float[] compute_singular_mesh_optimal(int number_of_solid_components) {
-        return get_alpha_shape_mesh("SINGULAR",
-                                    get_optimal_alpha(number_of_solid_components, ptr2cgalAlphaShape),
-                                    ptr2cgalAlphaShape);
-    }
-
-    public float alpha() {
-        return get_alpha(ptr2cgalAlphaShape);
-    }
-
-    public int number_of_solid_components() {
-        return number_of_solid_components(ptr2cgalAlphaShape);
-    }
-
-    public float get_optimal_alpha(int nb_sc) {
-        return get_optimal_alpha(nb_sc, ptr2cgalAlphaShape);
-    }
 }
